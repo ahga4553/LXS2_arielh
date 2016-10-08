8675c8f036390d77b21d7b7349b65dc11dbc9874
Ariel Gamez <ahga4553@gmail.com>
2016-10-08 09:59:00 -0600

    Uso de variable ERRORLOG

--- src/parametros_posicionales.sh
+++ src/parametros_posicionales.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+ERRORLO="$0.err"
 # La salida estandar de error estará redireccionada al archivo if-then.sh.err
 if cp archivo1.txt arcgivo1.txt.bck 2> $0.err
 then
 	echo "El archivo.txt fue correctante respaldado..."
+else
+	echo "El archivo.txt no pudo ser respaldado, verifique el log: $errorlog"
 fi
 
